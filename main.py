7c303000ebf71717645838488dbafc55c1d1a8ab
Dakota McGuire <30912971+saltwater-io@users.noreply.github.com>
2019-03-20 20:00:36 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from anytree import NodeMixin
+
+
+class Node:
+    parent = None
+    left = None
+    right = None
+    current_path = []
+
+    def __init__(self, parent, left, right):
+        self.parent = parent
+        self.left = left
+        self.right = right
+
+
+
+def insert(val):
+    if val <
+def main():
+    pass
+
+
+if __name__=="__main__":
+    main()

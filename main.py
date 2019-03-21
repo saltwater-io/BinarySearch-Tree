a828fab6c2af787cec7c200fe5218dfd8c0e7c1c
Dakota McGuire <30912971+saltwater-io@users.noreply.github.com>
2019-03-20 20:06:11 -0500

    update function placeholders

--- main.py
+++ main.py
@@ -5,17 +5,40 @@ class Node:
     parent = None
     left = None
     right = None
-    current_path = []
-
-    def __init__(self, parent, left, right):
+    data = None
+    def __init__(self, parent, left, right, data):
         self.parent = parent
         self.left = left
         self.right = right
 
 
 
-def insert(val):
-    if val <
+def insert(val, node):
+    if val < node.data:
+        pass
+    else:
+        pass
+
+def in_order():
+    pass
+
+
+def delete(node):
+    pass
+
+
+def search():
+    pass
+
+
+def get_min(node):
+    pass
+
+
+def get_max(node):
+    pass
+
+
 def main():
     pass
 
